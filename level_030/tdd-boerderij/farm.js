f9a7a5462d29ab94e85dec68c92933cf51f75b2b
Tristan Graaff <tristangraaff@yahoo.com>
2020-11-17 13:49:18 +0100

    get yield for plant function

---
+++ level_030/tdd-boerderij/farm.js
@@ -0,0 +1,11 @@
+const get_yield_for_plant = plant => plant.yield;
+
+// const get_yield_for_crop = () => {};
+
+// const get_total_yield = () => {};
+
+module.exports = {
+    get_yield_for_plant,
+    //get_yield_for_crop,
+    //get_total_yield,
+};
