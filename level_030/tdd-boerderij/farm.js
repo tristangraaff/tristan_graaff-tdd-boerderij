0cdd0f48212ecd2b87403a68a9d94c836cf15926
Tristan Graaff <tristangraaff@yahoo.com>
2020-11-18 10:31:56 +0100

    eerste tests en functies af

--- level_030/tdd-boerderij/farm.js
+++ level_030/tdd-boerderij/farm.js
@@ -1,11 +1,17 @@
-const get_yield_for_plant = plant => plant.yield;
+const get_yield_per_plant = plant => plant.yield;
 
-// const get_yield_for_crop = () => {};
+const get_total_yield_of_crops = crops => crops.num_plants * crops.crop.yield;
 
-// const get_total_yield = () => {};
+const get_total_yield = cropsArray => {
+    let total = 0
+    cropsArray.forEach(item => {
+        total = total + item.crop.yield * item.num_plants;
+    });
+    return total
+};
 
 module.exports = {
-    get_yield_for_plant,
-    //get_yield_for_crop,
-    //get_total_yield,
+    get_yield_per_plant,
+    get_total_yield_of_crops,
+    get_total_yield,
 };
